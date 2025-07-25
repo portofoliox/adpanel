1efc187991ec6d35265c4cb2aa9a61e2b9911ecc
Antonio Vatafu <137089208+portofoliox@users.noreply.github.com>
2025-07-25 15:05:53 +0300

    ADPanel for Termux

---
+++ bots/demo-bot/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World from ADPanel!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
